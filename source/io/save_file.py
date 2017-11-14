023f61b2467411fd83c8e145ead5357089546ecd
khardi <kharchilindanila@gmail.com>
2017-11-14 09:07:07 +0300

    join file examples

--- source/io/save_file.py
+++ source/io/save_file.py
@@ -1,13 +1,38 @@
-""""
+"""
 
 
 [title]: # (Пишем и читаем файл)
 
 
-Пишем и читаем файл
-===================
+Пишем и читаем json файл
+========================
+    
+    >>> import json
+    
+    >>> x = {
+    ...     'a': 1,
+    ...     'b': 2
+    ... }
+    
+    
+    # Пишем
+    >>> with open('x.json', 'w') as f:
+    ...     _ = f.write(json.dumps(x))
+    
+    
+    # Читаем
+    >>> with open('x.json', 'r') as f:
+    ...     z = json.load(f)
+    
+    
+    >>> print(isinstance(z, dict))
+    True
+    >>> print(z['a'])
+    1
 
 
+Пишем и читаем файл ключ-значение
+=================================
 Записывает и считывает простой файл.
 
     file sep.txt
@@ -19,12 +44,12 @@
     ...     ('a', 1),
     ...     ('b', 2)
     ... ]
-    
+
     # Пишем
     >>> with open('sep.txt', 'w') as f:
     ...     for key, value in x:
     ...         _ = f.write("{} {}\\n".format(key, value))
-    
+
     # Читаем
     >>> with open('sep.txt', 'r') as f:
     ...     for line in f:
